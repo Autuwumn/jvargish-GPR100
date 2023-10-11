ff0d4cbfee336c0e33aea3b2a7e3548d5764a093
Autuwumn <vargishjj@gmail.com>
2023-10-11 14:37:40 -0400

    TMP Importing
    added TMP and text in the ui

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,35 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject player;
     public GameObject coinPrefab;
+    public GameObject gameOverScreen;
     public List<GameObject> coins = new List<GameObject>();
-    private float spawnTimeGoal = 5, spawnTime = 0;
+    private float spawnTimeGoal = 3, spawnTime = 0;
     public int score;
+    public float fuel = 1000;
+    public TextMeshProUGUI scoreText, fuelText;
     void Start()
     {
-
+        gameOverScreen.SetActive(false);
     }
 
     void Update()
     {
+        scoreText.text = "Score: " + score;
+        fuelText.text = "Fuel: " + (int)fuel;
         spawnTime += Time.deltaTime;
         if(spawnTime > spawnTimeGoal)
         {
             spawnTime = 0;
             if (coins.Count < 5)
             {
-                var newCoin = Instantiate(coinPrefab);
-                newCoin.transform.position = new Vector3(Random.Range(-7f, 7f), Random.Range(-3f, 3f), -0.5f);
-                coins.Add(newCoin);
+                int scenario = Random.Range(0, 100);
+                var amount = 1;
+                if(scenario >= 99)
+                {
+                    amount = 10;
+                } else if(scenario >= 90)
+                {
+                    amount = 5;
+                } else if(scenario >= 60)
+                {
+                    amount = 2;
+                }
+                print(scenario);
+                for (int i = 0; i < amount; i++)
+                {
+                    var newCoin = Instantiate(coinPrefab);
+                    newCoin.transform.position = new Vector3(Random.Range(-7f, 7f), Random.Range(-3f, 3f), -0.5f);
+                    coins.Add(newCoin);
+                }
             }
         }
     }
     public void ScoreChange(int amt)
     {
         score += amt;
+    }
+    public void UsingFuel()
+    {
+        fuel -= 25 * Time.deltaTime;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameController gc;
+    public float speed;
+    public float drag = -0.2f;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        
+        var x = Input.GetAxis("Horizontal") * speed;
+        var y = Input.GetAxis("Vertical") * speed;
+        if(new Vector2(x,y).magnitude > 0.1f)
+        {
+            gc.UsingFuel();
+        }
+        rb.AddForce(new Vector2(x, y));
+        rb.AddForce(rb.velocity * drag);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<CoinController>() != null)
         {
             gc.ScoreChange(5);
+            gc.coins.Remove(collision.gameObject);
             Destroy(collision.gameObject);
         }
     }
